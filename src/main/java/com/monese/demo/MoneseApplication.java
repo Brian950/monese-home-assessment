bf1c4ab9af42cd0a824f4cd6a7c67d879574712a
Brian Heaphy <b5h@qad.com>
2021-10-01 19:21:38 +0100

    Initial commit - added spring skeleton

---
+++ src/main/java/com/monese/demo/MoneseApplication.java
@@ -0,0 +1,13 @@
+package com.monese.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneseApplication.class, args);
+	}
+
+}
